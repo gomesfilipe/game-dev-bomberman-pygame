45be03ab59d39fa711987a86f791e0842a779dbf
Filipe <filipejvmf@gmail.com>
2024-09-14 21:48:33 -0300

    fix: add type hint

--- src/enums/event_enum.py
+++ src/enums/event_enum.py
@@ -1,13 +1,13 @@
 from enum import Enum
-# from src.game_objects.game_object import GameObject
+from src.core.game_object import GameObject
 import pygame
 from typing import Dict, Callable
 
 class EventEnum(Enum):
   NEW_GAME_OBJECT = pygame.USEREVENT + 1
   DESTROY_GAME_OBJECT = pygame.USEREVENT + 2
-  QUIT = pygame.QUIT
-  PRESSED_KEY = pygame.KEYDOWN
+  QUIT = pygame.QUIT # Tratado pelo pygame
+  PRESSED_KEY = pygame.KEYDOWN # Tratado pelo pygame
 
   def post_event(self, **kwargs) -> None:
     post_handlers: Dict[EventEnum, Callable] = {
@@ -21,11 +21,11 @@ class EventEnum(Enum):
       self.__post_default_event()
 
   @classmethod
-  def __post_new_game_object_event(cls, game_object) -> None:
+  def __post_new_game_object_event(cls, game_object: GameObject) -> None:
     pygame.event.post(pygame.event.Event(cls.NEW_GAME_OBJECT.value, game_object = game_object))
 
   @classmethod
-  def __post_destroy_game_object_event(cls, game_object) -> None:
+  def __post_destroy_game_object_event(cls, game_object: GameObject) -> None:
     pygame.event.post(pygame.event.Event(cls.DESTROY_GAME_OBJECT.value, game_object = game_object))
 
   def __post_default_event(self) -> None:
