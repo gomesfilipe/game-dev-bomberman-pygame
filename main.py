d211b4def2b1d9d79fb1f2600439eeb8bcfe397d
Filipe <filipejvmf@gmail.com>
2024-09-14 21:45:34 -0300

    feat: remove observers

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src.games.bomberman_game import BombermanGame
+from src.core.game import Game
 from src.scenes.main_scene import MainScene
 from typing import Tuple, List
 import pygame
@@ -8,9 +8,5 @@ if __name__ == '__main__':
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
   scene = MainScene(screen, GAME_DURATION, 'wheat3')
 
-  game = BombermanGame(
-    scene,
-    observers = [],
-  )
-
+  game = Game(scene)
   game.run()
--- src/core/game.py
+++ src/core/game.py
@@ -1,64 +1,51 @@
 import pygame
-from typing import Optional, List, Dict, Callable
+from typing import Dict, Callable
 from src.core.scene import Scene
-from src.interfaces.observer_interface import ObserverInterface
 from src.enums.event_enum import EventEnum
 
-class Game(ObserverInterface):
+class Game():
   def __init__(
       self,
       scene: Scene,
-      observers: List[ObserverInterface] = [],
     ) -> None:
     self._scene = scene
-    self._observers = observers + [self]
     self._stop: bool = False
 
-    self._x: Optional[float] = None
-    self._y: Optional[float] = None
+    self._event_handlers = self._get_event_handlers()
+    self._key_handlers = self._get_key_handlers()
 
   def _should_stop(self) -> None:
     return self._stop
 
-  def handle_event(self, event: pygame.event.Event) -> None:
-    handlers: Dict[int, Callable] = {
-      EventEnum.QUIT.value: lambda: self.__handle_quit_event(event),
-      EventEnum.PRESSED_KEY.value: lambda: self.__handle_pressed_key_event(event),
-      EventEnum.NEW_GAME_OBJECT.value: lambda: self.__handle_new_game_object_event(event),
-      EventEnum.DESTROY_GAME_OBJECT.value: lambda: self.__handle_destroy_game_object_event(event),
+  def _get_event_handlers(self) -> Dict[int, Callable]:
+    return {
+      EventEnum.QUIT.value: lambda event: self.__handle_quit_event(event),
+      EventEnum.PRESSED_KEY.value: lambda event: self.__handle_pressed_key_event(event),
+      EventEnum.NEW_GAME_OBJECT.value: lambda event: self.__handle_new_game_object_event(event),
+      EventEnum.DESTROY_GAME_OBJECT.value: lambda event: self.__handle_destroy_game_object_event(event),
     }
 
-    handlers[event.type]()
+  def _get_key_handlers(self) -> Dict[int, Callable]:
+    return {
+      pygame.K_SPACE: lambda: self._scene.switch_debug_mode(),
+    }
+
+  def _handle_event(self, event: pygame.event.Event) -> None:
+    if event.type in self._event_handlers:
+      self._event_handlers[event.type](event)
 
   def __handle_quit_event(self, event: pygame.event.Event) -> None:
     self._stop = True
 
   def __handle_pressed_key_event(self, event: pygame.event.Event) -> None:
-    keys: Dict[int, Callable] = {
-      pygame.K_SPACE: lambda: self._scene.switch_debug_mode(),
-    }
-
-    if event.key in keys:
-      keys[event.key]()
+    if event.key in self._key_handlers:
+      self._key_handlers[event.key]()
 
   def __handle_new_game_object_event(self, event: pygame.event.Event) -> None:
     self._scene.add_game_object(event.game_object)
 
   def __handle_destroy_game_object_event(self, event: pygame.event.Event) -> None:
     self._scene.remove_game_object(event.game_object)
-
-  def interested_events(self) -> List[int]:
-    return [
-      EventEnum.QUIT.value,
-      EventEnum.PRESSED_KEY.value,
-      EventEnum.NEW_GAME_OBJECT.value,
-      EventEnum.DESTROY_GAME_OBJECT.value,
-    ]
-
-  def _handle_event(self, event: pygame.event.Event) -> None:
-    for observer in self._observers:
-      if event.type in observer.interested_events():
-        observer.handle_event(event)
 
   def run(self):
     self.__start()
@@ -77,3 +64,6 @@ class Game(ObserverInterface):
   def __handle_events(self) -> None:
     for event in pygame.event.get():
       self._handle_event(event)
+
+  def set_scene(self, scene: Scene) -> None:
+    self._scene = scene
